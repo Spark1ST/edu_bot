bf546ba272f5f701e14bfae4df7d029219b23fe4
Mohammed Hamad <108199617+Spark1ST@users.noreply.github.com>
2025-04-15 20:18:38 +0200

    Update firebase.py

--- fastpi/firebase.py
+++ fastpi/firebase.py
@@ -1,14 +1,26 @@
-# import firebase_admin
-# from firebase_admin import credentials, firestore, auth
+import firebase_admin
+from firebase_admin import credentials, firestore, auth
+import streamlit as st
 
-# def initialize_firebase():
-#     if not firebase_admin._apps:
-#         cred = credentials.Certificate("..\data\grad-51668-firebase-adminsdk-fbsvc-b04feff7eb.json")
-#         firebase_admin.initialize_app(cred)
+def initialize_firebase():
+    if not firebase_admin._apps:
+        firebase_key_str = st.secrets["firebase"]["firebase_admin_key"]
+        
+        # Parse the JSON string to a dictionary
+        try:
+            firebase_key_dict = json.loads(firebase_key_str)
+        except json.JSONDecodeError as e:
+            st.error(f"Failed to parse Firebase key. Check its format: {e}")
+            st.stop()
+        cred = credentials.Certificate(cred_dict)
+        
+        # Initialize Firebase
+        firebase_admin.initialize_app(cred)
     
-#     return {
-#         'auth': auth,
-#         'db': firestore.client()
-#     }
+    return {
+        'auth': auth,
+        'db': firestore.client()
+    }
 
-# firebase = initialize_firebase()
+# Usage
+firebase = initialize_firebase()
